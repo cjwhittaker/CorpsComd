d8da558c0f61807fd5fd873d836c1dce4edc76cf
cjwhittaker <30289411+cjwhittaker@users.noreply.github.com>
2017-12-22 12:14:27 +0000

    corrected indirect fire table

--- common_variables.vb
+++ common_variables.vb
@@ -14,7 +14,7 @@
     Public ph_units As Collection, enemy As Collection
     Public smokefiredthisturn As Boolean
     Public qtypinned As Integer, qtyrepulsed As Integer, qtydestroyed As Integer
-    Public topserial As Integer, oppfire As Boolean
+    Public topserial As Integer, oppfire As Boolean, result_option As String
     Public disorderedstatus As Color = Color.Green, lowammo As Color = Color.Blue, in_ds As Color = Color.Yellow, can_observe As Color = Color.YellowGreen, no_action_pts As Color = Color.LightGray
     Public nostatus As Color = Color.White, disruptedstatus As Color = Color.Red, dead As Color = Color.DarkGray, demoralisedstatus As Color = Color.DarkOrange, take_off As Color = Color.Aquamarine
     Public assaulting As Color = Color.LightSalmon
@@ -71,7 +71,7 @@
 {12, 5, 7, 9, 10, 0, 0, 0, 0, 0, 0, 0, 0},
 {13, 7, 9, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0}}
     Public indirect_fire_strength(,) As Integer =
-{{0, 0, 1, 1, 2, 3, 4, 5, 6, 7, 9, 10},
+{{0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
 {0, 1, 1, 2, 3, 4, 5, 6, 8, 9, 10, 11},
 {1, 1, 2, 3, 4, 5, 6, 8, 9, 9, 10, 11},
 {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12},
